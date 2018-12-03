7a146082518c46587686b2a72900d446a8aae201
Roosevelt Mesa <ram101@duke.edu>
2018-12-03 14:08:43 -0500

    Adding the code to the different scripts to execute regression

--- src/readData.R
+++ src/readData.R
@@ -1,3 +1,22 @@
-This is going to be my first attempt to do something :) 
+library(dplyr)
+library(readr)
+library(ggplot2)
+install.packages("fields")
+install.packages("lme4")
+library(fields)
+library(lme4)
 
-Typing commands just to get used to the steps required to commit the changes. 
+# Read in data
+# positional data about the RV Kahuna
+# kahuna <- read_csv('../../../presentations/2018/2018-12-03_DUML-RepResearch/data/2018-11-26_2017-Cape-Hatteras-BRS-kahuna-CEE.csv') 
+kahuna <- read_csv('data/2018-11-26_2017-Cape-Hatteras-BRS-kahuna-CEE.csv')
+kStart <- kahuna %>% 
+  filter(status == 'start')
+
+# Read in Gm182 Data: 100 estimated positions of Gm182, augmented with focal follow data
+gm182UP <- read_csv('data/2018-11-27_Gm182-UserPoints-Start-CEE-Locations-Kahuna.csv') %>% 
+  mutate(status = 'userPoints')
+
+# Read in Gm182 Data: 100 estimated positions of Gm182
+gm182 <- read_csv('data/2018-11-27_Gm182-Start-CEE-Locations-Kahuna.csv') %>% 
+  mutate(status = 'noUserPoints')
